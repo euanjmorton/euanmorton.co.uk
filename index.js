cfe537bf4140d4816e43e1d85b2f2f260e109452
Euan Morton <mail@euanmorton.co.uk>
2024-08-21 15:08:18 +0100

    Updated project structure + DB connection

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require("express")
 const app = express()
 const router = express.Router();
 
-// Handling GET / request 
+require('dotenv').config(); //.env file for config strings
+
+const mysql = require('mysql2');
+
+
+
+//remove MIME type executable error
+app.use(express.static(__dirname + "/public"))
+
 app.use("/", router);
 
 router.get("/", (req, res, next) => {
@@ -16,12 +24,29 @@ router.get("/hello", (req, res, next) => {
     res.send("This is the hello response");
 })
 
-router.get('/test', function(req,res){
-    console.log("hello test")
-    res.sendFile(__dirname + "/index.html");    
+router.get('/temps', function(req,res){
+    res.sendFile(__dirname + "/public/views/temperatureGraph.html");  
+    
+    var con = mysql.createConnection({
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        
+        con.query("SELECT * FROM Pinter_Pi.Temperature", function (err, result) {
+            if (err) throw err;
+            console.log("result",result);
+        });
+    });  
 })
 
 // Server setup 
-app.listen(3000, () => {
-    console.log("Server is Running on port 3000...")
+app.listen(80, () => {
+    console.log("Server is Running on port 80...")
 }) 
+   /* var port = process.env.PORT || 3000;
+    app.listen(port);*/
