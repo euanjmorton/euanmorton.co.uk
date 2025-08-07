56fef7a81f1f38bb3403c167fba3ec4785077909
Euan Morton <mail@euanmorton.co.uk>
2025-08-07 19:07:20 +0100

    Updated main page
    Improved Sidebar, added DaisyUI, added top navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import SideBar from "./components/SideBar";
+import SideBar from "./components/SideBar/SideBar";
+import NavBar from "./components/NavBar/NavBar";
 
 export default function Home() {
   return (
     <div>
       <main className="">
+        <NavBar></NavBar>
         <SideBar></SideBar>
       </main>
     </div>
