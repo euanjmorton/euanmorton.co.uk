76722b80ed2137d47eca226eb072deb20e024238
Euan Morton <mail@euanmorton.co.uk>
2025-08-15 11:01:25 +0100

    Temperature display
    Init display of temperature retrieved from DB

--- app/pinter/page.tsx
+++ app/pinter/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { getTemp } from "@/app/lib/temperatures";
 
-const PinterPage = () => {
+const PinterPage = async () => {
+  var currentTemperature = await getTemp();
+
   return (
     <>
       <div>
         <h2>Pinter Page</h2>
       </div>
+      <div>
+        <h3>Current Temperature:</h3>
+        <p>{currentTemperature}</p>
+      </div>
+      <div></div>
     </>
   );
 };
