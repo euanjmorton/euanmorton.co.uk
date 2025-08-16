1076ade894c98129473af55403726a836fb528a2
Euan Morton <mail@euanmorton.co.uk>
2025-08-16 14:05:59 +0100

    Updated files for build
    Pi now hosting app

--- app/lib/queryUtils.tsx
+++ app/lib/queryUtils.tsx
@@ -1,7 +1,7 @@
 import pool from "@/app/lib/db";
 import { RowDataPacket } from "mysql2/promise";
 
-export async function SelectQuery(queryString: string, params?: any[]) {
+export async function SelectQuery(queryString: string, params?: []) {
   const results = await pool.execute(queryString, params);
   return results as RowDataPacket[];
 }
